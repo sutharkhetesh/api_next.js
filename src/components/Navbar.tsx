f8adc7c72253508f07e00a725576992d6d66fca6
sutharkhetesh <sutharkhetesh165@gmail.com>
2023-09-19 22:25:36 +0530

    commited

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,17 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <ul className={styles.nav}>
       <li><Link href="#">LOGO</Link></li>
-        <li><Link href="#">Home</Link></li>
+        <li><Link href="#">Dashboard</Link></li>
         <li>
           <Link href="#">Books</Link>
           <ul>
-            <li><Link href="Books/Get">Get Data</Link></li>
-            <li><Link href="Books/Post">Post Data</Link></li>
-            <li><Link href="Books/Delete">Delete Data</Link></li>
-            <li><Link href="Books/Update">Patch Data</Link></li>
+            <li><Link href="Books/Get">Available Books</Link></li>
+            <li><Link href="Books/Post">Add Books</Link></li>
+            <li><Link href="Books/Delete">Remove Books</Link></li>
+            <li><Link href="Books/Update">Replace Books</Link></li>
           </ul>
         </li>
-        <li><Link href="#">Author</Link></li>
+        <li><Link href="#">About</Link></li>
         <li><Link href="#">Contact</Link></li>
       </ul>
     </nav>
